f37528995837fccc7aaba2e5e178e126ac847db9
elkyzeer <maksimkykla12@icluod.com>
2023-12-25 10:41:58 +0200

    module.cpp

---
+++ module.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+
+// Клас, який має метод brewCoffee()
+class CoffeeMaker {
+public:
+    void brewCoffee() {
+        std::cout << "Brewing coffee..." << std::endl;
+    }
+};
+
+// Клас, який має метод brewTea()
+class TeaMaker {
+public:
+    void brewTea() {
+        std::cout << "Brewing tea..." << std::endl;
+    }
+};
+
+// Інтерфейс, який використовується в CoffeeMaker, але потрібно адаптувати до TeaMaker
+class TeaAdapter : public CoffeeMaker {
+private:
+    TeaMaker teaMaker;
+
+public:
+    // Перевизначення методу brewCoffee() для виклику методу brewTea() через адаптер
+    void brewCoffee() override {
+        teaMaker.brewTea();
+    }
+};
+
+int main() {
+    // Створення екземплярів класів
+    CoffeeMaker coffeeMaker;
+    TeaAdapter teaAdapter;
+
+    // Виклик методу brewCoffee() через CoffeeMaker
+    coffeeMaker.brewCoffee();
+
+    // Виклик того ж методу через адаптер для TeaMaker
+    teaAdapter.brewCoffee();
+
+    return 0;
+}
